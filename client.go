4e470257dd52edf33688df12d3fdb1045b064e81
puslip41 <puslip41@gmail.com>
2016-10-27 17:29:17 +0900

    dladsfdsa

--- client.go
+++ client.go
@@ -2,43 +2,69 @@ package main
 
 import (
 	"crypto/tls"
-	"log"
 	"golang.org/x/crypto/pkcs12"
 	"io/ioutil"
 	"encoding/pem"
 	"fmt"
-	"github.com/puslip41/eStreamerClient/message"
 	"time"
 	"io"
 	"sync"
+	"os"
+
+	"github.com/puslip41/eStreamerClient/message"
+	"github.com/puslip41/eStreamerClient/configuration"
 )
 
+const FILE_END_FLAG = "END_FILE"
+const TIME_HOUR_PATTERN = "2006010215"
+
 var wg sync.WaitGroup
+
 func main() {
-	conn := connectServer()
+	config, err := configuration.ReadConfiguration()
+	if err != nil {
+		fmt.Println("cannot read config file:", err)
+		os.Exit(-1)
+	}
+
+	initializeDirectories(config)
+
+	configuration.InitializeLogger(config.LogLevel)
+	defer configuration.CloseLogger()
+
+	conn := connectServer(config)
 	if conn != nil {
 		defer conn.Close()
 
-		log.Println("client: connected to: ", conn.RemoteAddr())
+		configuration.WriteInfo("success connect server to %s", conn.RemoteAddr())
 
-		requestMessage := getRequestMessage()
-
-		n, err := conn.Write(requestMessage.Marshal())
-		if err != nil {
-			PrintError(err, "cannot send request message")
-		} else {
-			log.Printf("send request message: %d, % x", n, requestMessage.Marshal())
-		}
+		sendRequestMessage(conn)
 
 		wg.Add(3)
 
-		receiveQueue := receive(conn)
+		receiveQueue := receive(conn, config)
 
 		writeQueue := process(receiveQueue)
 
-		write(writeQueue)
+		write(config.ExportDirectory, writeQueue)
 
 		wg.Wait()
+	}
+}
+
+
+func IsExistDirectory(directory string) bool {
+	_, err := os.Open(directory)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func initializeDirectories(config configuration.Configuration) {
+	if IsExistDirectory(config.ExportDirectory) == false {
+		os.MkdirAll(config.ExportDirectory, os.FileMode(644))
 	}
 }
 
@@ -47,18 +73,18 @@ func readRawMessage(conn *tls.Conn) (message.RawMessage, error) {
 
 	_, err := conn.Read(messageHeader)
 	if err != nil {
-		PrintError(err, "cannot receive message header")
+		configuration.WriteError(err, "cannot receive message header")
+		return message.RawMessage{}, err
 	} else {
 		header := message.UnmarshalHeader(messageHeader)
-
-		//log.Printf("Header Version : %d, Type : %d, Message Length : %d\r\n", header.HeaderVersion, header.MessageType, header.MessageLength)
 
 		if header.MessageLength > 0 {
 			messageBody := make([]byte, header.MessageLength)
 
 			_, err = conn.Read(messageBody)
 			if err != nil {
-				PrintError(err, "cannot receive message body")
+				configuration.WriteError(err, "cannot receive message body")
+				return message.RawMessage{}, err
 			} else {
 				return message.RawMessage{Header:header, Content:messageBody}, nil
 			}
@@ -68,36 +94,56 @@ func readRawMessage(conn *tls.Conn) (message.RawMessage, error) {
 	return message.RawMessage{}, nil
 }
 
-func receive(conn *tls.Conn) <- chan *message.RawMessage {
+func sendRequestMessage(conn *tls.Conn) error {
+	requestMessage := getRequestMessage()
+	_, err := conn.Write(requestMessage.Marshal())
+	if err != nil {
+		configuration.WriteError(err, "cannot send request message")
+	} else {
+		configuration.WriteInfo("send request message")
+	}
+
+	return err
+}
+
+func receive(conn *tls.Conn, config configuration.Configuration) <- chan *message.RawMessage {
 	c := make(chan *message.RawMessage, 1024)
 
 	go func() {
 		defer close(c)
 		isConnected := true
+		var failedCount time.Duration
 		for {
 			if isConnected {
 				rawData, err := readRawMessage(conn)
 				if err != nil {
-					PrintError(err, "cannot read estreamer message")
+					failedCount++
 					if err == io.EOF {
 						isConnected = false
 						conn.Close()
+						configuration.WriteInfo("disconnect server")
+						time.Sleep(1000*1000*1000 * failedCount)
 					} else {
-
+						configuration.WriteError(err, "cannot read estreamer message")
 					}
 				} else {
 					if err = sendNullMessage(conn); err != nil {
-						PrintError(err, "cannot send null message")
+						configuration.WriteError(err, "cannot send null message")
 					}
+					failedCount = 0
 
 					c <- &rawData
 				}
 			} else {
-				conn := connectServer()
+				conn := connectServer(config)
 				if conn != nil {
 					isConnected = true
+					configuration.WriteInfo("reconnected to server: %s:%d", config.ServerIP, config.ServiecePort)
+					sendRequestMessage(conn)
 				} else {
-					time.Sleep(1000)
+					failedCount++
+					configuration.WriteError(nil, "cannot reconnect to server: %s:%d", config.ServerIP, config.ServiecePort)
+					time.Sleep(1000*1000*1000 * failedCount)
 				}
 			}
 		}
@@ -107,8 +153,8 @@ func receive(conn *tls.Conn) <- chan *message.RawMessage {
 	return c
 }
 
-func process(receiveQueue <- chan *message.RawMessage) <- chan fmt.Stringer {
-	c := make(chan fmt.Stringer, 1024)
+func process(receiveQueue <- chan *message.RawMessage) <- chan string {
+	c := make(chan string, 1024)
 
 	go func() {
 		defer close(c)
@@ -116,61 +162,95 @@ func process(receiveQueue <- chan *message.RawMessage) <- chan fmt.Stringer {
 			if v.Header.MessageType == message.EVENT_DATA {
 				recordHeader := message.UnmarshalRecordHeader(v.Content[:16])
 				if recordHeader.RecordType == 2 {
-					c <- message.UnmarshalPacketRecord(v.Header, recordHeader, v.Content[16:])
-				} else  {
-					log.Printf("undefined event data: RecordType:%d, RecordLength:%d, Data:% X\r\n", recordHeader.RecordType, recordHeader.RecordLength, v.Content[16:])
+					record := message.UnmarshalPacketRecord(v.Header, recordHeader, v.Content[16:])
+
+					intrusionEvent := message.GetIntrusionEvent(record.DeviceID, record.EventID)
+					if intrusionEvent != nil {
+						c <- fmt.Sprintf("%s packet_size=%d packet_data=%X", intrusionEvent.String(), record.PacketLength, record.PacketData)
+					} else {
+						configuration.WriteError(nil, "cannot find intrusion event record: device id:%d, event id:%d", record.DeviceID, record.EventID)
+					}
+
+				} else if recordHeader.RecordType == 104 {
+					v := message.UnmarshalIntrusionEventRecordIPv4(v.Header, recordHeader, v.Content[16:])
+					message.PutIntrusionEvent(v.DeviceID, v.EventID, v)
+				} else if recordHeader.RecordType == 105 {
+					v := message.UnmarshalIntrusionEventRecordIPv6(v.Header, recordHeader, v.Content[16:])
+					message.PutIntrusionEvent(v.DeviceID, v.EventID, v)
+				} else {
+					configuration.WriteWarning("undefined event data: RecordType:%3d, RecordLength:%3d, Data:% X\r\n", recordHeader.RecordType, recordHeader.RecordLength, v.Content[16:])
 				}
+
+				configuration.WriteDebug("### Record Storage Count: %d", message.GetCountInRecordStroage())
 			} else {
-				log.Printf("undefine data: %d\r\n", v.Header.MessageType)
+				configuration.WriteWarning("undefine data: % x, %d, % x\r\n", v.Header, len(v.Content), v.Content)
 			}
 		}
 		wg.Done()
-	} ()
+	}()
 
 	return c
 }
 
-func write(writeQueue <- chan fmt.Stringer) {
+func createLogFile(time time.Time, directory string) (*os.File, error) {
+	filename := fmt.Sprintf(`%s\%s.log`, directory, time.Format(TIME_HOUR_PATTERN))
+
+	return os.OpenFile(filename, os.O_CREATE | os.O_WRONLY | os.O_APPEND, os.FileMode(644))
+}
+
+func write(exportDirectory string, writeQueue <- chan string) {
 	go func () {
+		var file *os.File
+		var err error
+		beforeHour := ""
+
 		for v := range writeQueue {
-			//log.Printf("WRITE: %s", v.String())
-			v.String()
+			if beforeHour != time.Now().Format(TIME_HOUR_PATTERN) {
+				currentTime := time.Now()
+
+				if file != nil {
+					file.WriteString(FILE_END_FLAG)
+					file.Close()
+				}
+
+				file, err = createLogFile(currentTime, exportDirectory)
+				if err != nil {
+					configuration.WriteError(err, "cannot create log file:")
+				} else {
+					beforeHour = currentTime.Format(TIME_HOUR_PATTERN)
+				}
+			}
+			file.WriteString(v+"\n")
 		}
 		wg.Done()
 	}()
 }
 
-func PrintError(err error, format string, args ...interface{}) {
-	log.Printf("ERROR: %s\n%s", fmt.Sprintf(format, args...), err.Error())
-}
-
-func connectServer() *tls.Conn {
-	ip := "192.168.100.197"
-	port := "8302"
-	pkcs12FileName := `D:\SourceCode\Go\src\github.com\puslip41\eStreamerClient\certs\192.168.10.172.pkcs12`
-	pkcs12Password := "cisco123"
+func connectServer(config configuration.Configuration) *tls.Conn {
+	ip := config.ServerIP
+	port := config.ServiecePort
+	pkcs12FileName := config.Pkcs12FileName
+	pkcs12Password := config.Pkcs12Password
 
 	certPEMBlock, keyPEMBlock, err := readPkcs12(pkcs12FileName, pkcs12Password)
 	if err != nil {
-		PrintError(err, "cannot extract pem block")
+		configuration.WriteError(err, "cannot extract pem block")
 
 		return nil
 	}
 
 	cert, err := tls.X509KeyPair(certPEMBlock, keyPEMBlock)
 	if err != nil {
-		PrintError(err, "cannot make certification and key")
+		configuration.WriteError(err, "cannot make certification and key")
 		return nil
 	}
 
-	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+	tlsConfig := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
 
-	conn, err := tls.Dial("tcp", ip + ":" + port, &config)
+	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", ip, port), &tlsConfig)
 	if err != nil {
-		PrintError(err, "cannot connect server to %s:%s", ip, port)
+		configuration.WriteError(err, "cannot connect server to %s:%d", ip, port)
 		return nil
-	} else {
-		log.Printf("INFO: success connect server to %s:%s", ip, port)
 	}
 
 	return conn
@@ -181,13 +261,13 @@ func connectServer() *tls.Conn {
 func readPkcs12(pkcs12File, password string) ([]byte, []byte, error) {
 	data, err := ioutil.ReadFile(pkcs12File)
 	if err != nil {
-		PrintError(err, "cannot read pkcs12: %s", pkcs12File)
+		configuration.WriteError(err, "cannot read pkcs12: %s", pkcs12File)
 		return nil, nil, err
 	}
 
 	blocks, err := pkcs12.ToPEM(data, password)
 	if err != nil {
-		PrintError(err, "cannot convert pem block")
+		configuration.WriteError(err, "cannot convert pem block")
 		return nil, nil, err
 	}
 
